7fe7387b383cd74c6fb69b4ac204c69255fb4f40
SzymokArch <94990211+SzymokArch@users.noreply.github.com>
2023-05-25 17:57:13 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,51 +8,107 @@ Vector2 get_vec2(int x, int y, double length, double angle){
   return v2;
 }
 
+Vector2 point_looked(int x, int y, double angle, int map[16][16]){
+  double len = 0;
+  Vector2 vec = get_vec2(x, y, len, angle);
+  while (map[(int)(vec.y/64)][(int)(vec.x/64)] != 1){
+    vec = get_vec2(x, y, len, angle);
+    len ++;
+  }
+  return vec;
+}
+
+Vector2 * make_vector_array(double fov, short quantity, int y, int x, double angle, int map[16][16]){
+  Vector2 * vecarr;
+  vecarr = (Vector2 *)calloc(quantity, sizeof(Vector2));
+  double angl = angle - fov/2;
+  for (int i = 0; i < quantity; i ++){
+    vecarr[i] = point_looked(x, y, angl, map);
+    angl += fov/quantity;
+  }
+  return vecarr;
+}
+
+void update_vector_array(Vector2 * vecarr, double fov, short quantity, int y, int x, double angle, int map[16][16]){
+  double angl = angle - fov/2;
+  for (int i = 0; i < quantity; i ++){
+    vecarr[i] = point_looked(x, y, angl, map);
+    angl += fov/quantity;
+  }
+}
+
+void display_vector_array(Vector2 * vecarr, short quantity, int y, int x, double angle){
+  for (int i = 0; i < quantity; i ++){
+    DrawLine(x, y, vecarr[i].x, vecarr[i].y, {255, 0, 0, 100});
+  }
+}
+
 int main(int argc, char * argv[]){
-  InitWindow(1280, 720, "test");
-  SetTargetFPS(60);
-  int x = 25;
-  int y = 25;
+
+  int mapa[16][16] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+  };
+
+  int x = 720;
+  int y = 720;
   float angle = 3*M_PI/2;
-  Vector2 vec;
+  double fov = 1;
+  Vector2 * vecarr = make_vector_array(fov, 255, y, x, angle, mapa);
+
+  InitWindow(1024, 1024, "test");
+  SetTargetFPS(60);
   Vector2 dir;
-  Vector2 forward, backward, left, right;
   while (!WindowShouldClose()){
     BeginDrawing();
-      ClearBackground(WHITE);
-      
-      DrawCircle(x, y, 5, BLACK);
-      
-      vec = get_vec2(x, y, 100, angle);
-      DrawLine(x, y, vec.x, vec.y, RED);
+      ClearBackground(GRAY); 
+      for (int i = 0; i < 16; i ++){
+        for (int j = 0; j < 16; j ++){
+          if (mapa[i][j])
+            DrawRectangle(j * 64, i * 64, 62, 62, {0, 0, 0, 255});
+          else
+            DrawRectangle(j * 64, i * 64, 62, 62, {255, 255, 255, 255});
+        }
+      }
 
-      // dir = get_vec2(x, y, 7.5, angle);
-      // DrawLine(x, y, dir.x, dir.y, GREEN);
+      update_vector_array(vecarr, fov, 255, y, x, angle, mapa);
+      display_vector_array(vecarr, 255, y, x, angle);
 
-      forward = get_vec2(x, y, 2, angle);
-      DrawLine(x, y, forward.x, forward.y, RED);
-      backward = get_vec2(x, y, 2, angle - M_PI);
-      DrawLine(x, y, backward.x, backward.y, GREEN);
-      left = get_vec2(x, y, 2, angle - M_PI/2);
-      DrawLine(x, y, left.x, left.y, BLUE);
-      right = get_vec2(x, y, 2, angle + M_PI/2);
-      DrawLine(x, y, right.x, right.y, BROWN);
+      DrawCircle(x, y, 15, GREEN);
 
       if (IsKeyDown(KEY_W)){
-        x = round(forward.x);
-        y = round(forward.y);
+        dir = get_vec2(x, y, 5, angle);
+        x = round(dir.x);
+        y = round(dir.y);
       }
       if (IsKeyDown(KEY_S)){
-        x = round(backward.x);
-        y = round(backward.y);
+        dir = get_vec2(x, y, 5, angle - M_PI);
+        x = round(dir.x);
+        y = round(dir.y);
       }
       if (IsKeyDown(KEY_A)){
-        x = round(left.x);
-        y = round(left.y);
+        dir = get_vec2(x, y, 5, angle - M_PI/2);
+        x = round(dir.x);
+        y = round(dir.y);
       }
       if (IsKeyDown(KEY_D)){
-        x = round(right.x);
-        y = round(right.y);
+        dir = get_vec2(x, y, 5, angle + M_PI/2);
+        x = round(dir.x);
+        y = round(dir.y);
       }
 
       if (angle >= 2 * M_PI || angle <= -2 * M_PI){
@@ -62,7 +118,12 @@ int main(int argc, char * argv[]){
       if (IsKeyDown(KEY_LEFT))
         angle -= 0.05;
       if (IsKeyDown(KEY_RIGHT))
-        angle += 0.05;       
+        angle += 0.05;    
+      if (IsKeyDown(KEY_UP))
+        fov += 0.05;
+      if (IsKeyDown(KEY_DOWN))
+        fov -= 0.05;    
+
     EndDrawing();
   }
   return 0;
